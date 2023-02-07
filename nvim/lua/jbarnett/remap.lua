cbaf08ed5cf3972c26c57ec8fe77b0a8b0687db5
Jim Barnett <james.barnett.jr@gmail.com>
2023-02-07 08:56:34 -0600

    More keymaps

--- nvim/lua/jbarnett/remap.lua
+++ nvim/lua/jbarnett/remap.lua
@@ -1,6 +1,23 @@
 vim.g.mapleader = ","
 vim.keymap.set("n", "<leader>ff", vim.cmd.Ex)
 
+-- moves line down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- moves line up
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- go to end of file
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+-- go to beginning of file
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- a
+vim.keymap.set("n", "n", "nzzzv")
+
+vim.keymap.set("n", "N", "Nzzzv")
+
 
